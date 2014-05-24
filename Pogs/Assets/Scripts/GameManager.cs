85c2d8e18fe8110040b4f16a16cc11d426f23777
xenonsin <kenoiwasp@gmail.com>
2014-05-24 14:44:56 -0700

    hi

--- Pogs/Assets/Scripts/GameManager.cs
+++ Pogs/Assets/Scripts/GameManager.cs
@@ -3,10 +3,42 @@ using System.Collections;
 
 public class GameManager : MonoBehaviour {
 
+    public enum Turns
+    {
+        ATTACK,
+        DEFEND
+    }
+
+    private Turns _currentTurn;
+
 	// Use this for initialization
-	void Start () {
-	
-	}
+    IEnumerator Start()
+    {
+        while (true)
+        {
+            switch (_currentTurn)
+            {
+                case Turns.ATTACK:
+                    Attack();
+                    break;
+                case Turns.DEFEND:
+                    Defend();
+                    break;
+
+            }
+            yield return 0;
+        }
+    }
+
+    void Attack()
+    {
+
+    }
+
+    void Defend()
+    {
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
