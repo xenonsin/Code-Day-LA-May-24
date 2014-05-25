2f2aaa9b9816f24a5cdfbbfae7f592ba7d61495f
xenonsin <kenoiwasp@gmail.com>
2014-05-24 22:22:00 -0700

    added 3rd party

--- Pogs/Assets/Scripts/Wolf Scripts/Network Scripts/NetworkManager.cs
+++ Pogs/Assets/Scripts/Wolf Scripts/Network Scripts/NetworkManager.cs
@@ -7,7 +7,24 @@ public class NetworkManager : MonoBehaviour {
 	void Start () {
         PhotonNetwork.ConnectUsingSettings("0.1");
 	}
-	
+
+    void OnGUI()
+    {
+        GUILayout.Label(PhotonNetwork.connectionStateDetailed.ToString());
+    }
+
+    void OnJoinedLobby()
+    {
+        PhotonNetwork.JoinRandomRoom();
+    }
+
+    void OnPhotonRandomJoinFailed()
+    {
+        Debug.Log("Can't join random room!");
+        PhotonNetwork.CreateRoom(null);
+    }
+
+
 	// Update is called once per frame
 	void Update () {
 	
